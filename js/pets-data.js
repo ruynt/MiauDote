c7e9e5d96665c81ed9d899a14747beedadc9ad82
RuyNt <ruyneto.t@gmail.com>
2026-06-10 22:03:37 -0300

    feat: Da silva

--- js/pets-data.js
+++ js/pets-data.js
@@ -5,7 +5,7 @@ const allPets = [
   { id: 1, name: "Luna", age: 1, size: "pequeno", location: "Salvador - BA", especie: "gato", img: "./img/pets/luna.webp" },
   { id: 2, name: "Formiguinha", age: 3, size: "pequeno", location: "João Pessoa - PB", especie: "gato", img: "./img/pets/formiguinha.webp" },
   { id: 3, name: "Luke", age: 6, size: "pequeno", location: "Olinda - PE", especie: "cachorro", img: "./img/pets/luke.webp" },
-  { id: 4, name: "Sonim", age: 3, size: "pequeno", location: "João Pessoa - PB", especie: "gato", img: "./img/pets/sonim.webp" },
+  { id: 4, name: "Sonim da Silva", age: 3, size: "pequeno", location: "João Pessoa - PB", especie: "gato", img: "./img/pets/sonim.webp" },
   { id: 5, name: "Phoebe", age: 5, size: "medio", location: "João Pessoa - PB", especie: "cachorro", img: "./img/pets/phoebe.webp" },
   { id: 6, name: "Ara", age: 3, size: "pequeno", location: "João Pessoa - PB", especie: "gato", img: "./img/pets/ara.webp" }
 ];
